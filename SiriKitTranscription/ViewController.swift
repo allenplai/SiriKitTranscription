2cda4352e24a9ec31eeb6339313174f60a596136
Allen Lai <allenlai@Allens-MacBook-Pro.local>
2016-12-18 23:14:19 -0500

    bigger button

--- SiriKitTranscription/ViewController.swift
+++ SiriKitTranscription/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    var audioRec: AVAudioRecorder?
+    var recFileURL: URL!
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        checkPermissions()
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,139 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    func checkPermissions () {
+        if AVAudioSession.sharedInstance().recordPermission() != .granted {
+            requestRecordPermissions()
+        }
+        
+        if SFSpeechRecognizer.authorizationStatus() != .authorized {
+            requestTranscribePermissions()
+        }
+    
+    }
+    
+    func requestRecordPermissions() {
+        AVAudioSession.sharedInstance().requestRecordPermission { (allowed) in
+            if allowed {
+                
+            } else {
+                
+            }
+        }
+    }
 
+    func requestTranscribePermissions() {
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            if authStatus == .authorized {
+                
+            } else {
+                
+            }
+            
+            
+        }
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+
+         guard audioRec != nil else {
+            recordAudio()
+            return
+        }
+        if (audioRec?.isRecording)! {
+            audioRec?.stop()
+        } else {
+            recordAudio()
+        }
+        
+    }
+    
+    func recordAudio() {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        recFileURL = paths[0].appendingPathComponent("temp.mp4")
+        if FileManager.default.fileExists(atPath: recFileURL.absoluteString) {
+            do {
+                try FileManager.default.removeItem(atPath: recFileURL.absoluteString)
+            } catch {
+                
+            }
+        }
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .defaultToSpeaker)
+            try session.setActive(true)
+            let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                            AVSampleRateKey: 44100,
+                            AVNumberOfChannelsKey: 2,
+                            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                            ]
+            audioRec = try AVAudioRecorder(url: recFileURL, settings: settings)
+            audioRec?.delegate = self
+            audioRec?.record()
+            
+            self.recordButton.setTitle("stop", for: .normal)
+            
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+    func transcribeAudio() {
+        let recognizer = SFSpeechRecognizer()
+        let request = SFSpeechURLRecognitionRequest(url: recFileURL)
+        
+        recognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+            guard let result = result else {
+                return
+            }
+            
+            let text = result.bestTranscription.formattedString
+            self.textView.text = text
+            
+            if result.isFinal {
+                let text = result.bestTranscription.formattedString
+                self.textView.text = text
+            }
+            
+        })
+        
+        
+    }
+    
 }
+
+extension ViewController: AVAudioRecorderDelegate {
+
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        recordButton.setTitle("Record", for: .normal)
+        if flag {
+            // success
+
+            playAudio()
+            transcribeAudio()
+        } else {
+
+        }
+        
+    }
+
+    func playAudio () {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: recFileURL)
+            audioPlayer?.play()
+        }catch {
+            
+        }
+    }
+    
+}
+
+
+
+
+
 
